30eebc144186755aa6430fcf1daf358b92bd83d8
Sujeath Pareddy <p.sujeath@gmail.com>
2016-01-04 11:02:14 +0530

    Changed implementation to simpler 5 point least squares method

--- scleral.py
+++ scleral.py
@@ -14,10 +14,8 @@ import scipy as sc
 import scipy.misc
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
 from matplotlib.widgets import Cursor, Button
-from PIL import Image
-from ransac import RANSAC_circle
+from lsq import LSqFitCircle
 import math
 
 class scleralViewer(object):
@@ -25,10 +23,10 @@ class scleralViewer(object):
 	def __init__(self,img,frame_width = 30,frame_height = 30):
 		self.frame_width = frame_width
 		self.frame_height = frame_height
-		self.img = img[:,:,1] < 40
 		self.fig = plt.figure()
+		self.img = img
 		self.main_img = plt.subplot2grid((6,5),(0,0),rowspan = 6,colspan = 5)
-		k = self.main_img.imshow(self.img,cmap = plt.cm.gray)
+		self.main_img.imshow(self.img,cmap = plt.cm.gray)
 		
 		self.cursor = Cursor(self.main_img,useblit=False)
 		
@@ -36,6 +34,8 @@ class scleralViewer(object):
 		
 		self.lastclick_x = None
 		self.lastclick_y = None
+		
+		self.clickStack = []
 		
 	def onClick(self, event):
 		# Event has coordinate system with origin in the bottom left, y up and x right
@@ -46,19 +46,12 @@ class scleralViewer(object):
 			click_in_window_coords_y = event.y - self.main_img.bbox.bounds[1]
 			y = (click_in_window_coords_x)/self.main_img.bbox.bounds[2]*self.img.shape[1]
 			x = (self.main_img.bbox.bounds[3] - click_in_window_coords_y)/self.main_img.bbox.bounds[3]*self.img.shape[0]
-			# Get list of coordinates of all black points in a window around the clicked point
-			subimg = self.img[x-self.frame_height:x+self.frame_height,y-self.frame_width:y+self.frame_width]
-			f = plt.figure()
-			f.gca().imshow(subimg)
-			plt.show()
-			#TODO: Insert out of image guards ^
-			px,py = np.nonzero(subimg)
-			if len(px) >= 5:
-				points = np.matrix([px,py]).transpose() + np.matrix([x+self.frame_height,y+self.frame_width])
-				c = RANSAC_circle(points)
-				print (c.x,c.y,c.r)
-				print [x,y]
-				print math.sqrt((c.x-x)**2 + (c.y-y)**2)
+			self.clickStack.append([x,y])
+			if len(self.clickStack) == 5:
+				# Wait for 5 points are accumulated
+				circle = LSqFitCircle(np.matrix(self.clickStack))
+				print circle
+				self.clickStack = []
 		
 if __name__ == "__main__":
 	img = scipy.misc.imread('images/Tiff/cropped2_borders.tiff')
